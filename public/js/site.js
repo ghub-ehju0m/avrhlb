a9cec0d27fb76d04408e5c21fc99010c46f304fa
Victor <ghub-ehju0m@yandex.ru>
2016-12-19 20:41:59 +0700

    Landing update 3+

--- public/js/site.js
+++ public/js/site.js
@@ -1,17 +1,17 @@
 
-function MM_preloadImages(){var d=document;var p=d.MM_p=d.MM_p||[];for(var i=0,a=arguments;i<a.length;i++){var x=new Image();x.src=a[i];p.push(x);}}
+(function MM_preloadImages(){var d=document;var p=d.MM_p=d.MM_p||[];for(var i=0,a=arguments;i<a.length;i++){var x=new Image();x.src=a[i];p.push(x);}})('img/header_background.jpg');
 
-
-$(document).ready(function () {
-	MM_preloadImages('img/header_background.jpg');
-});
+/*###$(document).ready(function () {
+});*/
 
 
 // fadein header
 $(window).on('load', function () {
-	$('.loading_screen').fadeOut(400);
-	$('.pg').delay(800).fadeIn(400);
-	$('.pg-top').show().addClass('animated fadeInDownBig');
-	setTimeout(function(){$('.top-pic').show().addClass('animated fadeInUpBig');}, 1100);
-	setTimeout(function(){$('.top-txt').show().addClass('animated fadeInUpBig');}, 1300);
+	document.body.classList.remove('loading');
+
+	//$('.loading_screen').fadeOut(400);
+	//$('.pg').delay(800).fadeIn(400);
+	//$('.pg-top').show().addClass('animated fadeInDownBig');
+	//setTimeout(function(){$('.top-pic').show().addClass('animated fadeInUpBig');}, 1100);
+	//setTimeout(function(){$('.top-txt').show().addClass('animated fadeInUpBig');}, 1300);
 });
