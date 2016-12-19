7f8d360d6d15f61a5c90357f0507f47b55da0fd6
Victor <ghub-ehju0m@yandex.ru>
2016-12-19 21:05:41 +0700

    Landing update 3++

--- public/js/site.js
+++ public/js/site.js
@@ -1,17 +1,5 @@
-
 (function MM_preloadImages(){var d=document;var p=d.MM_p=d.MM_p||[];for(var i=0,a=arguments;i<a.length;i++){var x=new Image();x.src=a[i];p.push(x);}})('img/header_background.jpg');
 
-/*###$(document).ready(function () {
-});*/
-
-
-// fadein header
-$(window).on('load', function () {
+window.onload = function () {
 	document.body.classList.remove('loading');
-
-	//$('.loading_screen').fadeOut(400);
-	//$('.pg').delay(800).fadeIn(400);
-	//$('.pg-top').show().addClass('animated fadeInDownBig');
-	//setTimeout(function(){$('.top-pic').show().addClass('animated fadeInUpBig');}, 1100);
-	//setTimeout(function(){$('.top-txt').show().addClass('animated fadeInUpBig');}, 1300);
-});
+};
